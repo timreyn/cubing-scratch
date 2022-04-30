a36990354abbef2d7616529da475d27e96b4e6c5
Tim Reynolds <tim@cubingusa.org>
2022-04-29 22:00:31 -0400

    Add db.run().

--- py/db.py
+++ py/db.py
@@ -1,7 +1,24 @@
+from absl import flags
 import mysql.connector
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_bool('debug', False, 'Debug db queries.')
 
 db = mysql.connector.connect(
   host='localhost',
   user='root',
   password='',
   database='wca')
+
+def run(query, cursor=None):
+  if not cursor:
+    cursor = db.cursor()
+  if FLAGS.debug:
+    printed_query = '\n'.join([line.strip() for line in query.split('\n')])
+    print(printed_query)
+  cursor.execute(query)
+  for row in cursor.fetchall():
+    yield row
+  if FLAGS.debug:
+    print('%d rows returned.' % cursor.rowcount)
